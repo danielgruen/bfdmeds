21b7f9aef8f042f021635b628744b9a12a8ebb47
Daniel Gruen <dgruen@stanford.edu>
2017-02-12 14:47:51 -0800

    example

--- example.py
+++ example.py
@@ -6,5 +6,5 @@ import bfdmeds
 meds=bfdmeds.BFDMEDS("data_example/test_meds.fits", "data_example/test_meds_subtracted.fits", "data_example/test_meds_subtracted_replace_bad.fits")
 # give three MEDS files, regular, neighbor subtracted, neighbor subtracted + bad-pixel filled
 
-print meds.get_cutout_list(0, skip_coadd=True)
+print meds.get_cutout_list(0, type='image_mofsub', skip_coadd=True)
 # all get_*_list methods take an optional skip_coadd parameter; if true, the coadd stamp / coordinates / etc. are not in the returned list
