884df5d9040f63091478eddc89dd871baef0f0f0
Joe Zuntz <joezuntz@googlemail.com>
2017-02-12 17:01:53 -0800

    add docstrings

--- bfd_meds/psf.py
+++ bfd_meds/psf.py
@@ -11,20 +11,35 @@ import galsim.des
 import astropy.io.fits
 
 class PsfSource(object):
-    pass
+    """
+    A Base class representing sources of PSF information.
+
+    Subclasses are expected to override PsfSource.get_psf
+    See their docstrings for more info.
+    """
+    def get_psf(self, tilename, band, exposure, ccd, col, row, stamp_size, jacobian):
+        raise NotImplementedError("Need to implement get_psf in PsfSource objects")
+
 
 class MissingPSFError(StandardError):
+    """Error used when no PSF could be found for an exposure"""
     pass
 
 class TooManyPSFsError(StandardError):
+    """Error used when more than one PSF could be found for an exposure"""
     pass
 
 
 
 class LimitedSizeDict(collections.OrderedDict):
     """
-    A dictionary with a maximum size, suitable as a simple
-    Create as with a normal OrderedDict with the additional size_limit keyword arg.
+    A dictionary with a maximum size, suitable as a simple cache
+    
+    Parameters
+    ----------
+    Usual dictionary creation parameters plus:
+
+    psf_size: Maximum size after which objects are removed
 
     From http://stackoverflow.com/questions/2437617/limiting-the-size-of-a-python-dictionary
     """
@@ -34,6 +49,8 @@ class LimitedSizeDict(collections.OrderedDict):
         self._check_size_limit()
 
     def __setitem__(self, key, value):
+        #Remove and re-insert to update the cache
+        self.pop(key,None)
         collections.OrderedDict.__setitem__(self, key, value)
         self._check_size_limit()
 
@@ -45,10 +62,28 @@ class LimitedSizeDict(collections.OrderedDict):
 
 
 class PsfexSource(PsfSource):
+    """
+    Base class for PSFEx-related sources of PSF information.
+
+    Defines one method for internal use, _get_psf_data, which opens
+    a PSFEx FITS file and gets and caches PSF information from it.
+
+    Parameters
+    ----------
+    cache_size: number of instances of galsim.des.DES_PSFEx cached.
+
+    """
     def __init__(self, cache_size):
         self.cache = LimitedSizeDict(size_limit=cache_size)
 
     def _get_psf_data(self, fits_filename, hdu_name):
+        """
+        Check for the named hdu in the cache.  If found, return the 
+        cached HDU data.
+
+        Otherwise open the named fits file, get the data from the named
+        extension, and turn into a galsim.des.DES_PSFEx object.
+        """
         data = self.cache.get(hdu_name)
 
         if data is None:
@@ -83,6 +118,7 @@ class PsfexSource(PsfSource):
         """
         import galsim
         image = galsim.ImageD(nsidex, nsidey)
+
         #Note galsim uses 1-offset convention whereas coordinates in meds file are 0-offset:
         x_image_galsim = x_image+1
         y_image_galsim = y_image+1
@@ -93,6 +129,19 @@ class PsfexSource(PsfSource):
 
 
 class CollectedMedsPsfexSource(PsfexSource):
+    """
+    Class to represent PSFEx files collated into a single file
+    as a number of extensions.
+
+    Implemented get_psf so can be used by BFDMEDS.
+
+    Parameters
+    ----------
+    template: a string to be used as a template for filenames
+        for a given tilename and band, e.g. /path/to/psfs/{tilename}_{band}.fits
+
+    """
+
     def __init__(self, template, cache_size=25):
         super(CollectedMedsPsfexSource, self).__init__(cache_size)
         self.template = template
@@ -109,6 +158,33 @@ class CollectedMedsPsfexSource(PsfexSource):
 
 
     def get_psf(self, tilename, band, exposure, ccd, col, row, stamp_size, jacobian):
+        """
+        Get an image of the PSF for the given exposure and position
+
+        Parameters
+        ----------
+        tilename:
+            string, Identifier for the tile, used to find the correct collected PSF file
+        band:
+            string, Identifier for the band, used to find correct PSF
+        exposure:
+            string, Identifier for the exposure
+        ccd:
+            string or int, Identifier for the chip
+        col:
+            integer in MEDS indexing of the chip column to evaluate at
+        row:
+            integer in MEDS indexing of the chip row to evaluate at
+        stamp_size:
+            integer size of the psf image to return in pixels. Since PSFEx models
+            are stored in pixel space this implies a relative resolution
+        jacobian:
+            unused for now
+
+        Returns:
+            A numpy stamp_size x stamp_size image of the PSF
+
+        """
         fits_filename = self._get_psfex_filename(tilename, band)
         hdu_name = "{0}_{1}_c{2}".format(exposure, band, ccd)
         psf_data = self._get_psf_data(fits_filename, hdu_name)
@@ -118,7 +194,21 @@ class CollectedMedsPsfexSource(PsfexSource):
 
 
 class DirectoryPsfexSource(PsfexSource):
+    """
+    Class to represent PSFEx files stored in a single directory.
+    Implemented get_psf so can be used by BFDMEDS.
+
+    This version currently assumes it is isntantiated afresh for 
+    each tilename, so that the tile name is not used to form 
+    the path to search.
+
+    Parameters
+    ----------
+    directory: The name of the directory to search for .psf files.
+
+    """
     def __init__(self, directory, cache_size=25):
+
         super(DirectoryPsfexSource, self).__init__(cache_size)
         self.directory = directory
 
@@ -132,6 +222,33 @@ class DirectoryPsfexSource(PsfexSource):
         return files[0]
 
     def get_psf(self, tilename, band, exposure, ccd, col, row, stamp_size, jacobian):
+        """
+        Get an image of the PSF for the given exposure and position
+
+        Parameters
+        ----------
+        tilename:
+            string, Identifier for the tile, currently unused
+        band:
+            string, Identifier for the band, used to find correct PSF
+        exposure:
+            string, Identifier for the exposure
+        ccd:
+            string or int, Identifier for the chip
+        col:
+            integer in MEDS indexing of the chip column to evaluate at
+        row:
+            integer in MEDS indexing of the chip row to evaluate at
+        stamp_size:
+            integer size of the psf image to return in pixels. Since PSFEx models
+            are stored in pixel space this implies a relative resolution
+        jacobian:
+            unused for now
+
+        Returns:
+            A numpy stamp_size x stamp_size image of the PSF
+
+        """
         fits_filename = self._get_psfex_filename(exposure, band, ccd)
         hdu_name = "PSF_DATA"
         psf_data = self._get_psf_data(fits_filename, hdu_name)
